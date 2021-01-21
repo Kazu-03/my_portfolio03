62c3603abbeb9d24f41377b0219882e024cec258
Kazu-03 <sharehappy032523@gmail.com>
2021-01-22 00:26:48 +0900

    blogページの作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,40 @@
 class BlogsController < ApplicationController
-  def index
-    @blogs = Blog.all
+  def new
+    @blog = Blog.new
+    @blog.images.new
+  end
+
+  def create
+    @blog = Blog.create!(blog_params)
+    if @blog.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog.id)
+  end
+
+  def show
+    @blog =Blog.find(params[:id])
+  end
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to root_path
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :detatil, images_attributes:[:src])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'blogs#index'
+  resources :blogs, only: [:index,:new, :create, :destroy, :edit, :update, :show] do
+  end
 end
