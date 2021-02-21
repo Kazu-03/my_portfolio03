36073e40aeff2d2a70aaca5344bd0f211d5b9197
Kazu-03 <sharehappy032523@gmail.com>
2021-02-22 01:26:56 +0900

    slackset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
+  def slack_notification channel, text
+    return if Rails.env.development?
+
+    Slack.chat_postMessage(
+      channel: channel, text: text
+    )
+  end
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :image ])
